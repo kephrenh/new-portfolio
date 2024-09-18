bce9ce728c5251f559be63cc9f932e852713af31
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-09-18 22:53:14 +0200

    fix no-unused-var error

--- components/front/front-header.tsx
+++ components/front/front-header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+
 import { loginLinkData, navLinkData } from "@/lib/data/data";
 import { kaunshan } from "@/lib/fonts";
 import Link from "next/link";
@@ -7,7 +7,6 @@ import CustomLink from "../custom-link";
 import { Button } from "../ui/button";
 import { MenuIcon, X } from "lucide-react";
 
-import SocialIcons from "./social-icons";
 import { useDrawer } from "@/hooks/use-drawer";
 import MobileNavbar from "./mobile-navbar";
 
@@ -40,50 +39,7 @@ export default function FrontHeader() {
             </Button>
           </div>
         </div>
-        {/* <div className={isOpen ? "fixed left-0 top-0 w-full h-screen bg-black/70" : ""}>
-          <div
-            className={
-              isOpen
-                ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500 "
-                : "fixed left-[-100%] top-0 p-10 ease-in duration-500 "
-            }>
-            <div>
-              <div className="flex items-center justify-between wfull">
-                <Link href="/">
-                  <span className={`text-3xl text-[#5651e5] ${kaunshan.className}`}>MA</span>
-                </Link>
-                <Button variant={"outline"} onClick={handleToggleMenu}>
-                  <X size={25} />
-                </Button>
-              </div>
-              <div className="my-4 border-b border-gray-300">
-                <p className="w-[85%] md:w-[90%] py-4">
-                  {"Let's build something legendary together"}
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col py-4">
-              <ul className="uppercase">
-                {navLinkData.map(({ text, href }) => {
-                  return (
-                    <li
-                      key={"side-link_" + text}
-                      className="py-4 text-sm hover:text-[#5651e5] transition-all duration-300"
-                      onClick={handleToggleMenu}>
-                      <Link href={href}>{text}</Link>
-                    </li>
-                  );
-                })}
-              </ul>
-              <div className="pt-40">
-                <p className="uppercase tracking-widest text-[#5651e5]">{"Let's Connect"}</p>
-                <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
-                  <SocialIcons padding="p-3" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
+
         <MobileNavbar isOpen={isOpen} toggleMenu={handleToggleMenu} />
         <div>
           <ul className="text-sm flex items-center gap-4">
