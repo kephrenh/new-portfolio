5bb3596ebf5bf636c47e55314cfc566b830b3c9a
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-09-19 21:23:43 +0200

    add functions to utils file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const parseStringify = (data: any) => JSON.parse(JSON.stringify(data));
+
+export const ensureError = (value: unknown): Error => {
+  if (value instanceof Error) return value;
+
+  let stringified = "[Unable to stringify the thrown value]";
+
+  try {
+    stringified = JSON.stringify(value);
+  } catch {}
+  const error = new Error(`This value was thrown as is, not through an Error: ${stringified}`);
+  return error;
+};
