8f65811772fc61865a29496e664e45734cbf52ce
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-09-24 15:09:39 +0200

    add server actions to send email and register user

---
+++ app/actions/email.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import dbConnect from "@/lib/mongodb";
+import { reportError, getErrorMessage } from "@/lib/utils";
+import Client from "@/models/client.model";
+
+import { revalidatePath } from "next/cache";
+import { dataFormData } from "@/lib/data-formData";
+
+export async function sendEmail(formData: FormData) {
+  try {
+    const { name, email, phone, subject, message } = dataFormData(formData);
+
+    console.log("Form data:", {
+      name: name,
+      email: email,
+      phone: phone,
+      subject: subject,
+      message: message,
+    });
+
+    if (!email || !name || !phone || !subject || !message) {
+      throw new Error("All fields are required");
+    }
+
+    await dbConnect();
+    const filter = { email: email };
+    const updates = { messages: { subject: subject, body: message } };
+
+    const client = await Client.findOne(filter);
+
+    const newClient = new Client({
+      name: name,
+      email: email,
+      phone: phone,
+      messages: { subject: subject, body: message },
+    });
+
+    if (!client) {
+      Client.create(newClient);
+    } else {
+      await Client.findOneAndUpdate(filter, { $addToSet: updates }, { returnOriginal: false });
+    }
+
+    revalidatePath("/");
+  } catch (error) {
+    reportError({ message: getErrorMessage(error) });
+  }
+}
---
+++ app/actions/user.ts
@@ -0,0 +1,37 @@
+"use server";
+import { dataFormData } from "@/lib/data-formData";
+import dbConnect from "@/lib/mongodb";
+import { getErrorMessage, reportError } from "@/lib/utils";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+
+export const createUser = async (formData: FormData) => {
+  try {
+    const { firstName, lastName, email, password } = dataFormData(formData);
+
+    if (!firstName || !lastName || !email || !password) {
+      throw new Error("Missing required fields");
+    }
+
+    await dbConnect();
+    const filter = { email: email };
+
+    const foundUser = await User.findOne(filter);
+
+    if (foundUser) {
+      throw new Error("Un compte avec cet email existe déjà");
+    }
+
+    const hashedPwd = await bcrypt.hash(password, 10);
+    const userPayload = {
+      email: email,
+      firstName: firstName,
+      lastName: lastName,
+      password: hashedPwd,
+    };
+
+    await User.create(userPayload);
+  } catch (error) {
+    reportError({ message: getErrorMessage(error) });
+  }
+};
