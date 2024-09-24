eefcfbcad4a70216baac2d00b676763fff200a88
Mohamed Amoussa <mohamedj81@hotmail.com>
2024-09-24 14:49:49 +0200

    modify data.ts and icons.tsx

---
+++ data/data.ts
@@ -0,0 +1,102 @@
+const data = {
+  menu: [
+    {
+      text: "Accueil",
+      href: "/",
+    },
+    {
+      text: "A Propos",
+      href: "/a-propos",
+    },
+    {
+      text: "Projets",
+      href: "/projets",
+    },
+    {
+      text: "Contact",
+      href: "/contact",
+    },
+  ],
+
+  login: [
+    {
+      text: "Login",
+      href: "/connexion",
+    },
+    {
+      text: "Register",
+      href: "/creer-un-compte",
+    },
+  ],
+
+  social: [
+    {
+      text: "GitHub",
+      href: "https://github.com/kephrenh",
+      icon: "github",
+    },
+    {
+      text: "LinkedIn",
+      href: "https://www.linkedin.com/in/ymohamed-amoussa/",
+      icon: "linkedin",
+    },
+  ],
+
+  projects: [
+    {
+      title: "Projet 1",
+      github: {
+        url: "https://github.com/kephrenh/P8_Kasa",
+      },
+
+      demo: {
+        url: "https://kephenh-kasa.netlify.app/",
+      },
+      imageSrc:
+        "https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto:compress&cs:tinysrgb&w:1260&h:750&dpr:1",
+      description: "Description du projet 1",
+      stack: ["HTML", "SASS", "React JS"],
+    },
+    {
+      title: "Projet 2",
+      github: {
+        url: "https://github.com/kephrenh/P8_Kasa",
+      },
+      demo: {
+        url: "https://kephenh-kasa.netlify.app/",
+      },
+      imageSrc:
+        "https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto:compress&cs:tinysrgb&w:1260&h:750&dpr:1",
+      description: "Description du projet 2",
+      stack: ["HTML", "SASS", "React JS"],
+    },
+    {
+      title: "Projet 3",
+      github: {
+        url: "https://github.com/kephrenh/P8_Kasa",
+      },
+      demo: {
+        url: "https://kephenh-kasa.netlify.app/",
+      },
+      imageSrc:
+        "https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto:compress&cs:tinysrgb&w:1260&h:750&dpr:1",
+      description: "Description du projet 3",
+      stack: ["HTML", "SASS", "React JS"],
+    },
+    {
+      title: "Projet 4",
+      github: {
+        url: "https://github.com/kephrenh/P8_Kasa",
+      },
+
+      demo: { url: "https://kephenh-kasa.netlify.app/" },
+
+      imageSrc:
+        "https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg?auto:compress&cs:tinysrgb&w:1260&h:750&dpr:1",
+      description: "Description du projet 4",
+      stack: ["HTML", "SASS", "React JS"],
+    },
+  ],
+};
+
+export default data;
